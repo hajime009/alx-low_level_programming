140515a4d2157ba55b82a6510dacfa9ca7a19d4f
hajime009 <ennajynajib124@gmail.com>
2023-10-18 08:36:19 -0700

    more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Custom putchar function */
+int _putchar(char c);
+
+/* Function prototypes (add your function prototypes here) */
+/* Example: */
+void myFunction1(void);
+int myFunction2(int param);
+
+#endif /* MAIN_H */
+
